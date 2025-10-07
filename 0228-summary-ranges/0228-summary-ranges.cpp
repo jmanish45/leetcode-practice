b4fd89ac6174c5edbf4edd5cda37997fcfd9a8bc
Manish Jaiswal <jmanish2406@gmail.com>
2025-10-07 18:50:14 +0530

    Time: 0 ms (100%), Space: 9.3 MB (41%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-        int n = nums.size();
         vector<string> ans;
+        int n = nums.size();
         if(n==0) return ans;
         int start = nums[0];
-        
-        for(int i = 1; i<n; i++) {
-            if(nums[i]!=(nums[i-1]+1)) {
-                if(start==nums[i-1]) {
-                    ans.push_back(to_string(start));
+        for(int i = 1; i<n ; i++ ) {
+            if(nums[i]!=nums[i-1]+1) {
+                if(start!=nums[i-1] ){
+                    ans.push_back(to_string(start)+"->"+to_string(nums[i-1]));
                 }
                 else {
-                    ans.push_back(to_string(start)+"->"+to_string(nums[i-1]));
-                      
+                    ans.push_back(to_string(start));
                 }
-                start = nums[i]; 
+                start = nums[i];
             }
         }
-        if(start==nums[nums.size()-1]) {
+        //last range to print
+        if(start==nums[nums.size()-1]){
             ans.push_back(to_string(start));
         }
         else {
-            ans.push_back(to_string(start)+"->"+ to_string(nums[nums.size()-1]));
+            ans.push_back(to_string(start)+"->"+to_string(nums[n-1]));
+
         }
-    return ans;
+        return ans;
     }
 };
