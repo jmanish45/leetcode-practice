92e0267865c45a20c62985ec2a7c2ed2cd97ad27
Manish Jaiswal <jmanish2406@gmail.com>
2025-10-16 11:17:23 +0530

    Time: 0 ms (100%), Space: 17 MB (84.76%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
-    int operate(string s1, string s2, string o) {
-        int n1 = stoi(s1);
-        int n2 = stoi(s2);
+    int operate(int n1, int n2, string o) {
         int result = 0;
         if(o=="+") result = n1+n2;
         else if(o=="-") result = n1-n2;
@@ -12,22 +10,22 @@ public:
     }
     int evalRPN(vector<string>& tokens) {
         int n = tokens.size();
-        stack<string>st;
+        stack<int>st;
 
         for(int i=0; i<n; i++) {
             if(tokens[i]=="+" || tokens[i]=="*" || tokens[i]=="-" || tokens[i]=="/") {
-                    string n1 = st.top();
+                    int n1 = st.top();
                     st.pop();
-                    string n2 = st.top();
+                    int n2 = st.top();
                     st.pop();
                     int r = operate(n2, n1, tokens[i]);
-                    st.push(to_string(r));    
+                    st.push(r);    
             }
             else  {
-                st.push(tokens[i]);
+                st.push(stoi(tokens[i]));
             }
         }
-        int ans = stoi(st.top());
+        int ans = st.top();
         return ans;
     }
 
