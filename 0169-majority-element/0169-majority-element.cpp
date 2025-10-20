d636295c13d5b8cbe9791feffe8b0864163c8705
Manish Jaiswal <jmanish2406@gmail.com>
2025-10-20 12:54:30 +0530

    Time: 3 ms (29.96%), Space: 28.3 MB (7.79%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        map<int, int>mpp;
+        for(int i = 0; i<n; i++) {
+            mpp[nums[i]]++;
+            if(mpp[nums[i]]>n/2) return nums[i];
+        }
+    return -1;
+    }
+};
