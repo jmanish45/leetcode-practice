811dd70bc5a3cbd28ebbc59fcb6fecd33a9b3317
Manish Jaiswal <jmanish2406@gmail.com>
2025-10-20 14:11:36 +0530

    Time: 0 ms (100%), Space: 28.2 MB (28.65%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,12 +1,37 @@
+//First method : Timecompl = O(n) and Space Comp : O(n)  where n lies from k to n, k is unique elements in nums 
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         unordered_map<int, int>mpp;
+//         for(int i = 0; i<n; i++) {
+//             mpp[nums[i]]++;
+//             if(mpp[nums[i]]>n/2) return nums[i];
+//         }
+//     return -1;
+//     }
+// };
+
+//Seconf is the most optimal Approach with T.C : O(n) but with Space complexity : O(1) with using Boyer-Moore Voting Algorithm.
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        map<int, int>mpp;
+        int majority = 0;
+        int count = 0;
         for(int i = 0; i<n; i++) {
-            mpp[nums[i]]++;
-            if(mpp[nums[i]]>n/2) return nums[i];
+            if(count==0) {
+                majority  = nums[i];
+                count++;
+            }
+            else if(nums[i]==majority) {
+                count++;
+            }
+            else count--;
+
         }
-    return -1;
+        return majority;
     }
 };
