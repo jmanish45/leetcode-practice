26917bacaae570c12eeabce233e80add72713242
Manish Jaiswal <jmanish2406@gmail.com>
2025-10-30 23:34:33 +0530

    Time: 3 ms (1.34%), Space: 13.4 MB (6.93%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,29 +1,51 @@
 class Solution {
 public:
-    bool  search(vector<int> &arr, int target) {
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
         int lo = 0;
-        int hi = arr.size()-1;
+        int hi = (n*m) - 1;
         while(lo<=hi) {
-            int mid = lo + (hi-lo)/2;
-            if(target == arr[mid]) return true;
-            else if(target > arr[mid]) {
-                lo = mid + 1;
+            int mid_idx = lo + (hi-lo)/2;
+            int row = mid_idx/m;
+            int col = mid_idx % m;
+            int mid_val = matrix[row][col];
+            if(mid_val == target) return true;
+            else if (mid_val < target) {
+                lo = mid_idx + 1;
             }
-            else {
-                hi = mid - 1;
-            }
+            else hi = mid_idx - 1;
         }
         return false;
     }
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        bool result = false;
-        for(int i = 0; i<m; i++) {
-            if(target>= matrix[i][0] && target<=matrix[i][n-1]) {
-                 result = search(matrix[i], target);
-            }   
-        }
-        return result;
-    }
 };
+// };
+// class Solution {
+// public:
+//     bool  search(vector<int> &arr, int target) {
+//         int lo = 0;
+//         int hi = arr.size()-1;
+//         while(lo<=hi) {
+//             int mid = lo + (hi-lo)/2;
+//             if(target == arr[mid]) return true;
+//             else if(target > arr[mid]) {
+//                 lo = mid + 1;
+//             }
+//             else {
+//                 hi = mid - 1;
+//             }
+//         }
+//         return false;
+//     }
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+//         bool result = false;
+//         for(int i = 0; i<m; i++) {
+//             if(target>= matrix[i][0] && target<=matrix[i][n-1]) {
+//                  result = search(matrix[i], target);
+//             }   
+//         }
+//         return result;
+//     }
+// };
