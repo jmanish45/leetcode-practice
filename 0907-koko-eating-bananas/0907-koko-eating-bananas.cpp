3c01df3e6994f5967cea28a45f33dc9ee9e4a8d2
Manish Jaiswal <jmanish2406@gmail.com>
2025-11-21 18:57:44 +0530

    Time: 11 ms (41.67%), Space: 22.9 MB (44.8%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-    bool check(int speed, vector<int> &piles, int h) {
+    bool isPossible(int speed, vector<int>& piles, int h ) {
         int n = piles.size();
-        int count = 0;
-        for(int i = 0; i<n ; i++) {
-            if(count>h) return false;
-            if(speed >= piles[i]) count++;
-            else if(piles[i]%speed == 0) count += piles[i]/speed;
-            else {
-                count += (piles[i]/speed + 1);
+        int timereq = 0;
+        for(int i =0; i<n; i++) {
+            if(timereq>h) return false;
+            if(speed>=piles[i]) timereq+=1;
+            else  {
+                if(piles[i]%speed==0) timereq+=(piles[i]/speed);
+                else timereq+=((piles[i]/speed)+1);
             }
         }
-        if(count>h) return false;
+        if(timereq>h) return false;
         return true;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
         int n = piles.size();
         int mx = -1;
-        for(int pile : piles) {
-            mx = max(mx, pile);
+        for(int i=0; i<n; i++) {
+            mx = max(piles[i], mx);
         }
-        int lo = 1;
+        int lo = 1; //min speed will be 1 to eat atleast one banana in 1 hour
         int hi = mx;
-        int ans = -1;
+        int ans = 1;
         while(lo<=hi) {
-            int mid = lo + (hi-lo)/2;
-            if(check(mid, piles, h)==true) {
+            int mid = lo+(hi-lo)/2;
+            if(isPossible(mid, piles, h)) {
                 ans = mid;
-                hi = mid-1;
+                hi = mid - 1;
             }
             else {
-                lo = mid + 1;
+                lo = mid+1;
             }
-
         }
         return ans;
     }
