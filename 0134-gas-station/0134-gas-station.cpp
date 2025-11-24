a44cfba7603d6d53c3eb5881afd100dbe160e1b1
Manish Jaiswal <jmanish2406@gmail.com>
2025-11-24 11:49:24 +0530

    Time: 0 ms (100%), Space: 112.4 MB (84.94%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,28 +1,55 @@
+// class Solution {
+// public:
+//     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+//         int n = cost.size();
+//         int rem = 0;
+//         for(int i=0;i<n; i++) {
+//             if(gas[i]<cost[i]) continue;
+//             rem = gas[i]-cost[i];
+//             int j = (i+1)%n;
+//             while(j<n) {
+//                 if(rem+gas[j]>=cost[j]) rem+=(gas[j]-cost[j]);
+//                 else{
+//                     break;
+//                     rem = 0;
+//                 } 
+//                 j = (j+1)%n;
+//                 if(i==j) r+eturn i;
+//             }
+//         }
+//         return -1;
+
+//     }
+// };
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int totalgas = 0;
-        for(int i = 0; i<gas.size(); i++) {
-            totalgas+=gas[i];
-        }
+        int n = cost.size();
         int totalcost = 0;
-        for(int i = 0 ; i<cost.size(); i++) {
+        for(int i=0; i<n; i++) {
             totalcost+=cost[i];
         }
+        int totalgas = 0;
+        for(int i=0; i<n; i++) {
+            totalgas+=gas[i];
+        }
         if(totalgas<totalcost) return -1;
-        int i = 0;
-        int fuelrem = 0;
+        int rem = 0;
         int start = 0;
-        while(i<gas.size()) {
-            fuelrem += (gas[i]-cost[i]);
-            if(fuelrem<0) {
-                fuelrem = 0;
-                start = i+1;
+        int i = 0;
+        while(i<n) {
+            if(rem+gas[i]<cost[i]) {
+                rem = 0;
+                start=i+1;
+                
             }
-            
+            else {
+                rem+=(gas[i]-cost[i]) ;
+            }
+
             i++;
+
         }
         return start;
     }
-
 };
