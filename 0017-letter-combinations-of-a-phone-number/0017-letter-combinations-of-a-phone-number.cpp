0d0355267393d3d96615979d10cea73bd44b8f9a
Manish Jaiswal <jmanish2406@gmail.com>
2025-12-12 00:04:07 +0530

    Time: 0 ms (100%), Space: 10.1 MB (5.42%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    void solve(string digits, string output, vector<string>& ans, int idx, string mapping[] ){
+    void  solve(string digits, vector<string>& ans, string s, int idx, vector<string> map) {
         if(idx>=digits.length()) {
-            ans.push_back(output);
+            ans.push_back(s);
             return;
         }
-        
-        int number = digits[idx] - '0';
-        string value = mapping[number];
-        for(int i = 0; i<value.length(); i++) {
-            output.push_back(value[i]);
-            solve(digits, output, ans, idx+1, mapping);
-            output.pop_back();
+        int num = digits[idx]- '0';
+        string value = map[num];
+        for(int i=0; i<value.length(); i++) {
+            s.push_back(value[i]);
+            solve(digits, ans, s, idx+1, map);
+            s.pop_back();
         }
 
-    } 
+        
 
+    }
     vector<string> letterCombinations(string digits) {
+        int n = digits.size();
         vector<string> ans;
-        if(digits.length()==0) return ans;
-        string output="";
+        string s = "";
         int idx = 0;
-        string mapping[] = {"", "","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        solve(digits, output, ans, idx, mapping) ;
+        vector<string> map = {"", "", "abc", "def", "ghi" , "jkl", "mno", "pqrs", "tuv", "wxyz"} ;
+        solve(digits, ans, s,  idx, map);
         return ans;
     }
 };
