0d5bfb5fb1fffb102aa8ad219160db0bdf5caa11
Manish Jaiswal <jmanish2406@gmail.com>
2025-12-13 22:33:29 +0530

    Time: 881 ms (10.48%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,15 +1,15 @@
--- SELECT name as EMPLOYEE
--- FROM EMPLOYEE
--- WHERE salary > (
---     SELECT salary FROM EMPLOYEE 
---     where id = managerId
--- )
+SELECT e1.name as EMPLOYEE
+FROM EMPLOYEE as e1
+WHERE e1.salary > (
+    SELECT e2.salary FROM EMPLOYEE e2
+    where e2.id = e1.managerId
+)
 
-SELECT e1.name as Employee
-FROM Employee e1
-JOIN Employee e2
-on  e1.managerID = e2.id
-WHERE e1.salary > e2.salary
+-- SELECT e1.name as Employee
+-- FROM Employee e1
+-- JOIN Employee e2
+-- on  e1.managerID = e2.id
+-- WHERE e1.salary > e2.salary
 
 
 
