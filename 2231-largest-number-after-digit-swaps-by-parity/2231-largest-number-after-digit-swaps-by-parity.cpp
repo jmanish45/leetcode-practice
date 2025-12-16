4c3dc23b91560f0fc198b7955db31ecba21b907f
Manish Jaiswal <jmanish2406@gmail.com>
2025-12-16 19:38:18 +0530

    Time: 0 ms (100%), Space: 8.4 MB (64.76%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
@@ -1,8 +1,40 @@
+// class Solution {
+// public:
+//     int largestInteger(int num) {
+//        // num = 2314
+//        // swappig parity gives  = 4312
+//         vector<int> a ;
+//         while(num>0) {
+//             int digit = num%10;
+//             a.push_back(digit);
+//             num/=10;
+//         }
+//         reverse(a.begin(), a.end());
+//         //[1,2,3,4];
+//         int n = a.size();
+//         int i = 0;
+//         for(int i=0; i<n; i++) {
+//             for(int j=i+1; j<n; j++) {
+//                 if(a[i]%2==0  && a[j]%2==0) {
+//                     if(a[i]<a[j]) swap(a[i],a[j]);
+//                 }
+//                 else if(a[i]%2!=0 && a[j]%2!=0) {
+//                     if(a[i]<a[j]) swap(a[i],a[j]);
+//                 }
+//             }
+//         }
+//         int ans = 0;
+//         for(int i=0; i<n; i++) {
+//             ans*=10;
+//             ans+=a[i];
+//         }    
+//     return ans;
+//     }
+// };
+
 class Solution {
 public:
     int largestInteger(int num) {
-       // num = 2314
-       // swappig parity gives  = 4312
         vector<int> a ;
         while(num>0) {
             int digit = num%10;
@@ -10,28 +42,32 @@ public:
             num/=10;
         }
         reverse(a.begin(), a.end());
-        //[1,2,3,4];
+        
         int n = a.size();
-        int i = 0;
+        
+        priority_queue<int> even;
+        priority_queue<int> odd;
+        for(int i=0;i<n; i++) {
+            if(a[i]%2==0) even.push(a[i]);
+            else odd.push(a[i]);
+        }
         for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                if(a[i]%2==0  && a[j]%2==0) {
-                    if(a[i]<a[j]) swap(a[i],a[j]);
-                }
-                else if(a[i]%2!=0 && a[j]%2!=0) {
-                    if(a[i]<a[j]) swap(a[i],a[j]);
-                }
+            if(a[i]%2==0) {
+                a[i] = even.top();
+                even.pop();
+            }
+            else {
+                a[i] = odd.top();
+                odd.pop();
             }
         }
+        
 
         int ans = 0;
         for(int i=0; i<n; i++) {
             ans*=10;
             ans+=a[i];
-        }
-
-
-        
+        }    
     return ans;
     }
 };
