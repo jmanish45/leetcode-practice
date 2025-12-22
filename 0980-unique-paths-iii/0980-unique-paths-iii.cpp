2a6e754f923aca482cf46d0b9b0887be22730a3f
Manish Jaiswal <jmanish2406@gmail.com>
2025-12-22 21:42:34 +0530

    Time: 0 ms (100%), Space: 9.2 MB (89.67%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -1,43 +1,46 @@
 class Solution {
 public:
-    int m, n,  nonobstacles, result;
-    vector<vector<int>> dir = {{-1,0}, {0,-1}, {1,0}, {0,1}};
-    void backtrack(vector<vector<int>>& grid, int count,int i,int j) {
+    int m, n, nonobstacle, result ;
+    vector<vector<int>> dir {{-1,0}, {0,-1}, {1,0}, {0,1}};
+    void backtrack(vector<vector<int>>& grid, int i, int j, int count) {
         if(i<0 || j<0 || i>=m || j>=n || grid[i][j]==-1) {
             return ;
         }
-        if(grid[i][j]==2) {
-            if(count==nonobstacles) result++;
+        if(grid[i][j]==2 ) {
+            if(count==nonobstacle) {
+                result++;
+            }
             return;
         }
+        int temp = grid[i][j];
         grid[i][j] = -1;
         for(auto& d : dir) {
-            int new_i = i+d[0];
-            int new_j = j+d[1];
-            backtrack(grid, count+1, new_i, new_j);
+            int ni = i+d[0];
+            int nj = j+d[1];
+            backtrack(grid, ni, nj, count+1);
         }
-        grid[i][j] = 0;
-        
+        grid[i][j]=temp;
     }
     int uniquePathsIII(vector<vector<int>>& grid) {
         m = grid.size();
         n = grid[0].size();
-        nonobstacles = 0;
+        nonobstacle = 1 ; //as the first element whhich is the start point is also a nonsobstacle
+        result = 0;
         int start_x = 0;
         int start_y = 0;
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(grid[i][j]==0) {
-                    nonobstacles++;
+                    nonobstacle++;
                 }
                 if(grid[i][j]==1) {
-                    nonobstacles++;
                     start_x = i;
                     start_y = j;
+                    
                 }
             }
         }
-        backtrack(grid, 0, start_x, start_y);
+        backtrack(grid,start_x, start_y, 0 );
         return result;
     }
 };
