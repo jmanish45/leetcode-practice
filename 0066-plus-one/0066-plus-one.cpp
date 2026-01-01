ec171936bd6c4e38c4f15d0ad4ca4c4cd5b3087d
Manish Jaiswal <jmanish2406@gmail.com>
2026-01-01 20:35:27 +0530

    Time: 0 ms (100%), Space: 11.4 MB (77.72%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,29 +2,28 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
+        vector<int> result(n,0);
         int carry = 1;
-        for(int i = n-1; i>=0; i--){
-            int new_digit = digits[i]+carry;
-            if(new_digit>=10) {
-                digits[i]=0;
+        
+        for(int i=n-1; i>=0; i--) {
+            int d = digits[i]+carry;
+            if(d>9) {
                 carry = 1;
-
-            }
+                result[i] = d%10;
+            } 
             else {
-                digits[i]=new_digit;
                 carry = 0;
-                break;
+                result[i] = d;
             }
         }
-        if(carry == 1) {
-            vector<int> new_digits(n+1,0) ;
-                new_digits[0]=1;
-                for(int i = 1; i<n+1;i++) {
-                    new_digits[i]=digits[i-1];
-                }
-                return new_digits;
-            
+        vector<int> result2(n+1, 0);
+        result2[0] = 1;
+        if(carry==1) {
+            for(int i=1 ; i<=n; i++) {
+                result2[i] = result[i-1];
+            }
+            return result2;
         }
-        return digits;
+        return result;
     }
 };
