fc13e8e7a90ce48851d375489deedbfe732dde27
Manish Jaiswal <jmanish2406@gmail.com>
2026-01-10 23:18:17 +0530

    Time: 47 ms (13.7%), Space: 10.5 MB (94.09%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int helper(string& a, int i, string& b, int j) {
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(a[i]==b[j]) return dp[i][j] = helper(a, i-1, b, j-1);
+    int dp[500][500];
+    int helper(string& word1, int m, string& word2, int n) {
+        if(m<0) return n+1;
+        if(n<0) return m+1;
+        if(dp[m][n]!=-1) return dp[m][n];
+        if(word1[m]==word2[n]) return dp[m][n] = helper(word1, m-1, word2, n-1);
         else {
-            int add = 1 + helper(a, i, b, j-1);
-            int remove = 1 + helper(a, i-1, b , j);
-            int replace = 1 + helper(a, i-1, b, j-1);
-            return dp[i][j] = min(add, min(remove, replace));
-        }
+            int add = 1 + helper(word1, m, word2,  n-1);
+            int remove = 1 + helper(word1, m-1, word2, n);
+            int replace = 1 + helper(word1, m-1, word2, n-1);
+            return dp[m][n]=min(add, min(remove, replace));
+        } 
     }
     int minDistance(string word1, string word2) {
         int m = word1.size();
         int n = word2.size();
-        dp.resize(m, vector<int>(n, -1));
+        memset(dp, -1, sizeof(dp));
         return helper(word1, m-1, word2, n-1);
     }
 };
