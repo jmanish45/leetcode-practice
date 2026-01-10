8aff80cf6002b4cc50de753b3d063435b96d0e2e
Manish Jaiswal <jmanish2406@gmail.com>
2026-01-10 21:04:13 +0530

    Time: 27 ms (37.69%), Space: 13.4 MB (82.33%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    
-    int lcs(string &s1, int m, string &s2, int n) {
-        if (m == 0 || n == 0) return 0;  // no ASCII sum if one string is empty
-        if (dp[m][n] != -1) return dp[m][n];
-        
-        if (s1[m-1] == s2[n-1]) {
-            // Add ASCII value of the matching char
-            return dp[m][n] = lcs(s1, m-1, s2, n-1) + (int)s1[m-1];
-        } else {
+    int dp[1000][1000];
+    int lcs(string& s1, int m, string& s2, int n) {
+        if(m==0 || n==0) return 0;
+        if(dp[m][n]!=-1) return dp[m][n];
+        if(s1[m-1]==s2[n-1]) {
+            return dp[m][n] = lcs(s1, m-1, s2, n-1) + int(s1[m-1]);
+        }
+        else {
             return dp[m][n] = max(lcs(s1, m-1, s2, n), lcs(s1, m, s2, n-1));
         }
     }
-    
     int minimumDeleteSum(string s1, string s2) {
-        int w1 = s1.size(), w2 = s2.size();
-        dp.assign(w1+1, vector<int>(w2+1, -1));
-        
-        int totalSum = 0;
-        for (char c : s1) totalSum += (int)c;
-        for (char c : s2) totalSum += (int)c;
-        
-        int commonSum = lcs(s1, w1, s2, w2);
-        return totalSum - 2 * commonSum;
+        int l1 = s1.length();
+        int l2 = s2.length();
+        //we will take totalsum of all the characters ascii numbers
+        //then we with the help of recursion we can check for the maximum possible common substring which is common in both the strings
+        //then we will find the answer by subtracting the sum of ascii values of common substrings from the totalsum 
+        int totalsum = 0;
+        memset(dp, -1, sizeof(dp));
+        for(char c : s1) totalsum+= int(c);
+        for(char c : s2) totalsum+= int(c);
+
+        int commonsum = lcs(s1,l1, s2,l2);
+        return totalsum - 2*(commonsum);
+
     }
 };
