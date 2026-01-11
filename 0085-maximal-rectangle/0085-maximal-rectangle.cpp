517f3817921d0cb6b9e7d0d4acb21c932ae0a78c
Manish Jaiswal <jmanish2406@gmail.com>
2026-01-12 00:17:50 +0530

    Time: 15 ms (16.54%), Space: 21.1 MB (17.41%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int m, n, ma;
-    int solve(vector<int>& h) {   //this  function relates to the question Largest Rectangle in histogram LC QUE 84
+    int m, n, maxarea;
+    int solve(vector<int>& helper) {   //this  function relates to the question Largest Rectangle in histogram LC QUE 84
         vector<int> nsi(n);
         nsi[n-1] = n;
         stack<int> sn;
         sn.push(n-1);
         for(int i=n-2; i>=0 ; i--) {
-            while(sn.size()>0 && h[sn.top()]>=h[i]) sn.pop();
+            while(sn.size()>0 && helper[sn.top()]>=helper[i]) sn.pop();
             if(sn.size()==0) nsi[i] = n;
             else nsi[i] = sn.top(); 
             sn.push(i);
@@ -17,14 +17,14 @@ public:
         stack<int> sp;
         sp.push(0);
         for(int i=1; i<n; i++) {
-            while(sp.size()>0 && h[sp.top()]>=h[i]) sp.pop();
+            while(sp.size()>0 && helper[sp.top()]>=helper[i]) sp.pop();
             if(sp.size()==0) psi[i] = -1;
             else psi[i] = sp.top();
             sp.push(i);
         }
         int maxa = 0; 
         for(int i=0; i<n; i++) {
-            int height = h[i];
+            int height = helper[i];
             int width = nsi[i]-psi[i]-1;
             int area = height*width;
             maxa = max(area, maxa);
@@ -35,19 +35,19 @@ public:
     int maximalRectangle(vector<vector<char>>& mat) {
         m = mat.size();
         n = mat[0].size();
-        ma = 0;
-        vector<int> h (n, 0);
+        maxarea = 0;
+        vector<int> helper (n, 0);
 
         for(int i = 0; i<m; i++) {
             for(int j = 0; j<n; j++) {
-                if(mat[i][j]=='0') h[j] = 0;
-                else h[j]+=1;    
+                if(mat[i][j]=='0') helper[j] = 0;
+                else helper[j]+=1;    
             }
-            int currarea = solve(h);
-            ma = max(currarea, ma);
+            int currarea = solve(helper);
+            maxarea = max(currarea, maxarea);
             
         }
-        return ma;
+        return maxarea;
 
     }
 };
