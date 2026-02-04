8a7542d91e7bb222915b65bc343c2146fb9cb6ca
Manish Jaiswal <jmanish2406@gmail.com>
2026-02-04 16:37:38 +0530

    Time: 0 ms (100%), Space: 10.6 MB (56.85%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,13 +1,26 @@
+// class Solution {
+// public:
+//     vector<int> countBits(int n) {
+//         vector<int> ans(n+1);
+//         for(int i=0; i<=n; i++) {
+//             int ones = 0;
+//             for(int j=0; j<31; j++) {
+//                 if(i & (1<<j)) ones++;
+//             }
+//             ans[i] = ones;
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> ans(n+1);
-        for(int i=0; i<=n; i++) {
-            int ones = 0;
-            for(int j=0; j<31; j++) {
-                if(i & (1<<j)) ones++;
-            }
-            ans[i] = ones;
+        if(n==0) return ans;
+        ans[0] = 0;
+        for(int i=1; i<=n; i++) {
+            if(i%2==1) ans[i] = ans[i/2]+1;
+            else ans[i] = ans[i/2];
         }
         return ans;
     }
