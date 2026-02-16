09c50eb46b9c54a0bb0fb2f886eed28dbe14046a
Manish Jaiswal <jmanish2406@gmail.com>
2026-02-16 23:32:51 +0530

    Time: 0 ms (100%), Space: 8.2 MB (46.07%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        string binary(32, '0');
+        int i = 0;
+
+        while(n > 0 && i < 32) {
+            int rem = n % 2;
+            binary[i] = rem + '0';
+            i++;
+            n = n / 2;
+        }
+
+        uint32_t ans = 0;
+        uint32_t mult = 1;
+
+        for(int i = 31; i >= 0; i--) {
+            ans += mult * (binary[i] - '0');
+            mult *= 2;
+        }
+
+        return ans;
+    }
+};
