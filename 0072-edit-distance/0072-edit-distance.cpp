656a8357957122280877461729c12edc92f41367
Manish Jaiswal <jmanish2406@gmail.com>
2026-03-18 01:12:14 +0530

    Time: 103 ms (5.11%), Space: 82 MB (10.14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    int dp[500][500];
-    int helper(string& word1, int m, string& word2, int n) {
+    int dp[501][501];
+
+    int solve(string word1, string word2, int m, int n) {
         if(m<0) return n+1;
         if(n<0) return m+1;
         if(dp[m][n]!=-1) return dp[m][n];
-        if(word1[m]==word2[n]) return dp[m][n] = helper(word1, m-1, word2, n-1);
+        if(word1[m]==word2[n]) return dp[m][n] =  solve(word1, word2, m-1, n-1);
         else {
-            int add = 1 + helper(word1, m, word2,  n-1);
-            int remove = 1 + helper(word1, m-1, word2, n);
-            int replace = 1 + helper(word1, m-1, word2, n-1);
-            return dp[m][n]=min(add, min(remove, replace));
-        } 
+            int add = 1 + solve(word1, word2, m-1, n);
+            int remove = 1 + solve(word1, word2, m, n-1);
+            int replace = 1 + solve(word1, word2, m-1, n-1);
+
+            return dp[m][n] = min(add, min(remove, replace));
+
+
+        }
     }
     int minDistance(string word1, string word2) {
         int m = word1.size();
         int n = word2.size();
-        memset(dp, -1, sizeof(dp));
-        return helper(word1, m-1, word2, n-1);
+        memset(dp, -1, sizeof(dp)) ;
+        return solve(word1, word2, m-1, n-1);
     }
 };
