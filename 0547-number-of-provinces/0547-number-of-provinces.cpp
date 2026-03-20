e757e4a8ef5723ba4b291d0566dcedf39ccef72a
Manish Jaiswal <jmanish2406@gmail.com>
2026-03-20 20:04:47 +0530

    Time: 9 ms (7.31%), Space: 20 MB (45.29%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
-    void dfs(int node,  vector<int> list[], vector<bool> &visit) {
-        visit[node] = 1;
-        for(auto it : list[node]) {
-            if(visit[it]==0) {
-                dfs(it,  list, visit);
+    void dfs(int node, vector<int> list[], vector<bool>& visited) {
+        visited[node] = true;
+        for(auto &neigh : list[node]) {
+            if(visited[neigh]==false) {
+                dfs(neigh, list, visited);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& matrix) {
-        int v = matrix.size();
+        int v = matrix.size() ;// v -> no of total vertices
+
+        //create an array of vectors to store all the vertices with their neighbours
         vector<int> list[v];
-        for(int i = 0; i<v; i++){
-            for(int j = 0; j<v; j++){
+
+        for(int i=0; i<v; i++) {
+            for(int j = 0; j<v; j++) {
                 if(matrix[i][j]==1 && i!=j && i<j) {
                     list[i].push_back(j);
                     list[j].push_back(i);
+
                 }
             }
         }
-        vector<bool> visit(v, false);
+
+        //now from about iterations all the vertices are stored with theiri connected nodes
+
+        //now to count the no of seperate connectiojs using dfs 
+
         int count = 0;
-        for(int i=0; i<v ; i++) {
-            if(visit[i]==false) {
+        vector<bool> visited(v, false);
+        for(int i = 0; i<v; i++) {
+            if(visited[i]==0) {
                 count++;
-                dfs(i, list, visit);
+                dfs(i, list, visited);
             }
+
         }
+
         return count;
     }
 };
