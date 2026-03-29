cf47e3a253b337d06847764f131a3c46e60caf5b
Manish Jaiswal <jmanish2406@gmail.com>
2026-03-29 19:52:40 +0530

    Time: 3 ms (40.01%), Space: 20.4 MB (39.18%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
-    void dfs(int node, vector<int> list[], vector<bool>& visited) {
-        visited[node] = true;
-        for(auto &neigh : list[node]) {
-            if(visited[neigh]==false) {
-                dfs(neigh, list, visited);
-            }
+    void dfs(int i, unordered_map<int, vector<int>>& adj,  vector<bool>& visited) {
+        visited[i] = true;
+        for(auto& neig : adj[i]) {
+            if(!visited[neig]) dfs(neig, adj, visited);
         }
     }
-    int findCircleNum(vector<vector<int>>& matrix) {
-        int v = matrix.size() ;// v -> no of total vertices
-
-        //create an array of vectors to store all the vertices with their neighbours
-        vector<int> list[v];
-
-        for(int i=0; i<v; i++) {
-            for(int j = 0; j<v; j++) {
-                if(matrix[i][j]==1 && i!=j && i<j) {
-                    list[i].push_back(j);
-                    list[j].push_back(i);
-
-                }
+    int findCircleNum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        unordered_map<int, vector<int>> adj;
+        for(int i = 0; i<n; i++) {
+            for(int j=0; j<n ; j++) {
+                if(mat[i][j]==1 && i!=j) {
+                    adj[i].push_back(j);
+                }   
             }
         }
-
-        //now from about iterations all the vertices are stored with theiri connected nodes
-
-        //now to count the no of seperate connectiojs using dfs 
-
         int count = 0;
-        vector<bool> visited(v, false);
-        for(int i = 0; i<v; i++) {
-            if(visited[i]==0) {
+        vector<bool> visited(n, false) ;
+        for(int i=0; i<n; i++) {
+            if(!visited[i]) {
                 count++;
-                dfs(i, list, visited);
+                dfs(i, adj, visited);
             }
-
         }
-
-        return count;
+        return count ;
     }
 };
