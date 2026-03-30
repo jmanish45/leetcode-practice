072c568ee1b77eeb84524d4e8354b7449067153f
Manish Jaiswal <jmanish2406@gmail.com>
2026-03-30 20:12:01 +0530

    Time: 0 ms (100%), Space: 14.1 MB (72.75%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,50 +1,66 @@
 class Solution {
 public:
-    void dfs(int row, int col, vector<vector<char>>& board, vector<vector<int>>& visit) {
-        visit[row][col] = 1;
-        int m = board.size();
-        int n  = board[0].size();
-        int drow[] = {-1, 0, +1, 0};
-        int dcol[] = {0, +1, 0, -1};
-        for(int i = 0; i<4 ; i++) {
-            int nrow = row + drow[i];
-            int ncol = dcol[i] + col;
-            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && board[nrow][ncol]=='O' && visit[nrow][ncol]==0 ) {
-                dfs(nrow, ncol, board, visit);
+    int m, n;
+    vector<vector<int>> dir = {{-1,0},{1,0},{0,1},{0,-1}};
+    void dfs(int i, int j, vector<vector<char>>& board) {
+        if(i<0 || j<0 || i>=m || j>=n || board[i][j]=='X') {
+            return;
+        }
+        board[i][j]='S';
+        for(int k=0; k<4; k++) {
+            int ni = i+dir[k][0];
+            int nj = j+dir[k][1];
+            if(ni>=0 && nj>=0 && ni<m && nj<n && board[ni][nj]=='O') {
+                dfs(ni,nj,board);
             }
         }
+        return;
     }
     void solve(vector<vector<char>>& board) {
-        int m = board.size();
-        int n = board[0].size();
-        vector<vector<int>> visit(m, vector<int> (n,0));
-        //traverse first row and last row
-        for(int  j = 0; j<n; j++) {
-            //first row 
-            if(board[0][j]=='O' && visit[0][j]==0) {
-                dfs(0, j, board, visit);
-            }
-            //last row
-            if(board[m-1][j]=='O' && visit[m-1][j]==0) {
-                dfs(m-1, j, board, visit);
+        m = board.size();
+        n = board[0].size();
+       //we will heck all the border rows and columsn if there is a 'O' then the dfs will be applied to the neighbour 'O' of it if present and they will be marksed as safe with replacing it with 'S';
+
+        //Top Row
+        for(int i=0; i<n; i++) {
+            if(board[0][i]=='O') {
+                dfs(0,i,board);
             }
         }
-        //traverse first and last column
-        for(int i = 0; i<m; i++) {
-            if(board[i][0]=='O' && visit[i][0]==0) {
-                dfs(i, 0, board, visit);
-            }
-            if(board[i][n-1]=='O' && visit[i][n-1]==0) {
-                dfs(i, n-1, board, visit);
+
+        //Bottom Row
+        for(int i=0; i<n; i++) {
+            if(board[m-1][i]=='O') {
+                dfs(m-1,i,board);
             }
         }
-        for(int i = 0; i<m ;i++) {
-            for(int j = 0; j<n; j++) {
-                if(board[i][j]=='O' && !visit[i][j]) {
-                    board[i][j] = 'X';
+
+        //Left column
+        for(int i=0; i<m; i++) {
+            if(board[i][0]=='O') {
+                dfs(i,0,board);
+            }
+        }
+
+        //Right column
+        for(int i=0; i<m; i++) {
+            if(board[i][n-1]=='O') {
+                dfs(i, n-1, board);
+            }
+        }
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(board[i][j]=='S') {
+                    board[i][j]='O';
+                }
+                else if(board[i][j]=='O') {
+                    board[i][j]='X';
                 }
             }
         }
+        return;
+
 
     }
 };
