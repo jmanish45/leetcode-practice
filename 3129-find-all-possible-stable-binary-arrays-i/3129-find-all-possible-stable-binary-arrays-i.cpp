dd8b4ae1cbc5bdfa7b0194dea9c84cd22e7bae76
Manish Jaiswal <jmanish2406@gmail.com>
2026-06-26 12:25:49 +0530

    Time: 233 ms (40.25%), Space: 8.8 MB (81.73%) - LeetHub

--- 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.cpp
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    int M = 1e9 + 7;
     int t[201][201][2];
-
-    int solve(int zeroleft, int oneleft, bool isLastOne, int limit) {
-        if(zeroleft == 0 && oneleft == 0) {
-            return 1;
-        }
-
-        if(t[zeroleft][oneleft][isLastOne] != -1) {
-            return t[zeroleft][oneleft][isLastOne];
-        }
-
-        long long result = 0;
-
-        if(isLastOne) {
-            for(int len = 1; len <= min(zeroleft, limit); len++) {
-                result = (result + solve(zeroleft-len, oneleft, false, limit)) % M;
+    int mod = 1e9+7;
+    int solve(int zeroleft, int oneleft, bool lastwasone, int limit) {
+        if(zeroleft==0 && oneleft==0) return 1;
+        if(t[zeroleft][oneleft][lastwasone]!=-1) 
+            return t[zeroleft][oneleft][lastwasone];
+        int result=0;
+        if(lastwasone) {
+            for(int len = 1 ; len<=min(zeroleft , limit); len++) {
+                result = (result+solve(zeroleft-len, oneleft, false, limit))%mod;
             }
-        } 
+        }
         else {
-            for(int len = 1; len <= min(oneleft, limit); len++) {
-                result = (result + solve(zeroleft, oneleft-len, true, limit)) % M;
+            for(int len = 1; len<=min(oneleft, limit); len++) {
+                result =( result + solve(zeroleft, oneleft-len, true, limit))%mod;
             }
         }
-
-        return t[zeroleft][oneleft][isLastOne] = result;
+        return t[zeroleft][oneleft][lastwasone] = result;
     }
-
     int numberOfStableArrays(int zero, int one, int limit) {
         memset(t, -1, sizeof(t));
-
-        int startWithOne = solve(zero, one, false, limit);
-        int startWithZero = solve(zero, one, true, limit);
-
-        return (startWithOne + startWithZero) % M;
+        int startwithzero = solve(zero , one, true, limit);
+        int startwithone = solve(zero, one, false, limit);
+        
+        return (startwithzero + startwithone)%mod ; 
     }
 };
