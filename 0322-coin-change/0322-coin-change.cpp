ca1c50605fda47ea434a1c0acc13a5f98dc0901e
Manish Jaiswal <jmanish2406@gmail.com>
2026-07-15 20:58:57 +0530

    Time: 70 ms (6.9%), Space: 50 MB (15.17%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
+    typedef long long ll;
     vector<vector<int>> dp;
     int n;
-    int helper(int idx, long long sum, int amount, vector<int>& coins) {
+    int solve(int idx, vector<int>& coins, int amount) {
         if(idx==n) {
-            if(sum==amount) return 0;
+            if(amount==0) return 0;
             else return INT_MAX;
         }
-        if(dp[idx][sum]!=-1) return dp[idx][sum];
-        if(sum+coins[idx] > amount) {
-            return dp[idx][sum] = helper(idx+1, sum, amount, coins);
+        if(dp[idx][amount]!=-1) return dp[idx][amount];
+        if((amount-coins[idx])<0) {
+            return solve(idx+1, coins, amount);
         }
         else {
-            long long take = 1LL + helper(idx, sum+coins[idx], amount, coins);
-            long long leave = helper(idx+1, sum, amount, coins);
-            return dp[idx][sum]  = min(take, leave);
+            ll take = (ll)1 + solve(idx, coins, amount-coins[idx]);
+            ll leave = solve(idx+1, coins, amount);
+            return dp[idx][amount] = min(take, leave);
         }
+
         
     }
     int coinChange(vector<int>& coins, int amount) {
         n = coins.size();
-        dp.resize(n,  vector<int> (amount+1, -1));
-        int ans = helper(0, 0, amount,coins);
-        if(ans==INT_MAX) return -1;
-        return ans;
+        dp.resize(n, vector(amount+1, -1));
+        int ans =  solve(0, coins, amount);
+        return ans==INT_MAX ? -1 : ans;
     }
 };
