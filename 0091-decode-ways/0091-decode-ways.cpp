3170d232b14fafacddd7e1084c85c4e511374b68
Manish Jaiswal <jmanish2406@gmail.com>
2026-07-16 02:19:16 +0530

    Time: 2 ms (22.47%), Space: 11.3 MB (7.07%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int n;
+    int dp[101];
+    int solve(int i, string s) {
+        if(dp[i]!=-1) return dp[i];
+        if(i==n) return dp[i]=1;
+        if(s[i]=='0') return 0;
+        int result = solve(i+1,s);
+        if(i+1<n) {
+            if(s[i]=='1' || s[i]=='2' && s[i+1]<='6') {
+                result+=solve(i+2,s);
+            }
+        }
+        return dp[i]=result;
+    }
+    int numDecodings(string s) {
+        n = s.length();
+        memset(dp,-1,sizeof(dp));
+        return solve(0,s);
+    }
+};
