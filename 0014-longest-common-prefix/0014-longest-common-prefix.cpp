163c97e4d96aa68c2bb668497a68c75747a003fb
Manish Jaiswal <jmanish2406@gmail.com>
2026-07-28 02:32:20 +0530

    Time: 0 ms (100%), Space: 11.9 MB (39.08%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,6 +3,7 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         int n = strs.size();
         if(n==1) return strs[0];
+        
         sort(strs.begin(),strs.end());
         string first = strs[0];
         string last = strs[n-1];
