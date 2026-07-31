d0e6e4ad215a6089835ebc828e5b87b56aab7a2b
Manish Jaiswal <jmanish2406@gmail.com>
2026-07-31 15:18:19 +0530

    Time: 0 ms (100%), Space: 8.2 MB (77.71%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,19 +1,31 @@
+// class Solution {
+// public:
+//     int minimumPushes(string word) {
+//         int n = word.length();
+//         if(n<=8) return n;
+//         n-=8;
+//         int ans = 8;
+//         int add = 2;
+//         while(n>0) {
+//             if(n>=8) ans+=add*8;
+//             else ans+=add*n;
+//             n-=8;
+//             add++;
+//         }
+//         return ans;
+
+
+//     }
+// };
+
 class Solution {
 public:
     int minimumPushes(string word) {
-        int n = word.length();
-        if(n<=8) return n;
-        n-=8;
-        int ans = 8;
-        int add = 2;
-        while(n>0) {
-            if(n>=8) ans+=add*8;
-            else ans+=add*n;
-            n-=8;
-            add++;
+        int n = word.size();
+        int ans = 0 ;
+        for(int i=0; i<n; i++) {
+            ans+=(i/8)+1;
         }
         return ans;
-
-
     }
 };
