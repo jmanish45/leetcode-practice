a5b19f7973e5ead0d9a3d4cb5ab7733f1a27ac21
Manish Jaiswal <jmanish2406@gmail.com>
2026-08-11 00:56:06 +0530

    Time: 41 ms (27.25%), Space: 151.9 MB (7.91%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int n;
     vector<int> t;
+    vector<bool> visit;
     int solve(int idx, vector<int>& stone) {
         if(idx>=n) return 0;
-        if(t[idx]!=-1) return t[idx];
+        if(visit[idx]) return t[idx];
+        visit[idx] = true;
         int result = stone[idx]-solve(idx+1, stone);
         if(idx+1<n) {
             result = max(result, stone[idx]+stone[idx+1] - solve(idx+2, stone));
@@ -21,6 +23,7 @@ public:
         //will find the diff of the score of Alice and Bod
         //diff = Alice - Bob
         t.resize(n+1,-1);
+        visit.resize(n+1, false);
         int diff = solve(0,stone);
         
         if(diff>0) {
