3dd3406c9f6464223b136616fe2c7ebe18327677
Manish Jaiswal <jmanish2406@gmail.com>
2026-09-09 09:55:50 +0530

    Time: 0 ms (100%), Space: 9 MB (84.8%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -3,19 +3,14 @@ public:
     long long countCommas(long long n) {
         long long ans = 0;
         long long start = 1000;
-        long long commas = 1;
-
-        while (start <= n) {
-            long long next = start * 1000;
-
-            long long end = min(n, next - 1);
-
-            ans += (end - start + 1) * commas;
-
+        long long comma = 1;
+        while(start<=n) {
+            long long next = start*1000;
+            long long end = min(n, next-1);
+            ans += (end-start+1) * comma;
             start = next;
-            commas++;
+            comma++;
         }
-
         return ans;
     }
 };
