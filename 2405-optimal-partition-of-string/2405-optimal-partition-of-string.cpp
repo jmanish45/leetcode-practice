6064906fc2f895ff8ac920b205f40b57544fb841
Manish Jaiswal <jmanish2406@gmail.com>
2026-09-10 13:07:22 +0530

    Time: 8 ms (65.9%), Space: 13 MB (92.54%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int partitionString(string s) {
+        int n = s.length();
+        int count = 1;
+        int start = -1 ;
+        int i = 0;
+        int last = 0;
+        vector<int> mp(26, -1);
+        while(i<n) {
+            if(mp[s[i]-'a']!=-1 && mp[s[i]-'a']>start) {
+                count++;
+                start = i-1;
+            }
+            mp[s[i]-'a'] = i;
+            i++;
+        }
+        return count;
+    }
+};
