12006ba98f35292b7c6b92a53e641a7c9cb54b9c
Manish Jaiswal <jmanish2406@gmail.com>
2026-09-24 10:52:00 +0530

    Time: 787 ms (5%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,1 +1,5 @@
-SELECT distinct author_id as id from views where author_id = viewer_id order by author_id;
+SELECT DISTINCT
+author_id as id
+from Views
+where author_id = viewer_id
+ORDER BY author_id
